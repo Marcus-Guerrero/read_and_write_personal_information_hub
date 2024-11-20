8a1de1238935de3ecc74f15284ace7608c2eeef5
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-11-20 22:20:35 +0800

    Adding Pseodocode

---
+++ read_and_write_personal_info_of_users.py
@@ -0,0 +1,6 @@
+#Open a text file
+#Place a loop
+#Create five input statements
+#Create conditions to end the loop
+#Place a command that ask users if they want to input a new entry, if yes, new loop, if no, end the loop and write entry on the text file
+
